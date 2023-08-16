dcec33a078ff2121e04e68e6aa18b5dcff18728e
Guilherme <guilhermeoc.m@gmail.com>
2023-08-16 03:14:32 -0300

    listaVazia(ok) quantidadeInvalidade(ok) itemInvalido(ok) itemExtra(ok)

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -32,11 +32,19 @@ class CaixaDaLanchonete {
     return false
   }
 
-  calcularValorDaCompra(metodoDePagamento, itens) {
-      if(this.verificarAdicional(itens)){
-          return "Item extra não pode ser pedido sem o principal"
+  verificarItensInexistentes(itens){
+    let codigoCardapio = ['cafe', 'chantily', 'suco', 'sanduiche', 'queijo', 'salgado', 'combo1', 'combo2']
+    for(let n of itens){
+      let codigoSeparadoQuantidade = n.split(',')
+      if(!codigoCardapio.includes(codigoSeparadoQuantidade[0])){
+        return true
       }
+    }
+    return false
+  }
 
+  calcularValorDaCompra(metodoDePagamento, itens) {
+      
       if(this.verificarListaVazia(itens)){
           return "Não há itens no carrinho de compra!"
       }
@@ -44,6 +52,12 @@ class CaixaDaLanchonete {
       if(this.verificarItemZero(itens)){
           return "Quantidade inválida!"
       }
+      if(this.verificarItensInexistentes(itens)){
+        return "Item inválido!"
+      }
+      if(this.verificarAdicional(itens)){
+        return "Item extra não pode ser pedido sem o principal"
+    }
       
       return 'passou'
     }
