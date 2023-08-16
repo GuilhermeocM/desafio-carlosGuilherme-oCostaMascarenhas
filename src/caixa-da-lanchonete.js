80b38e2c144aa9f54ae8132ce43828bd18d834f2
Guilherme <guilhermeoc.m@gmail.com>
2023-08-16 02:29:10 -0300

    primeiro commit do desafio

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,7 +1,51 @@
 class CaixaDaLanchonete {
 
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+  verificarAdicional(itens){
+      let itensSeparadosDeQuantidade = itens.join(',').split(',')
+
+      if (itensSeparadosDeQuantidade.includes('chantily') && !itensSeparadosDeQuantidade.includes('cafe')){
+        return true
+      }
+      if (itensSeparadosDeQuantidade.includes('queijo') && !itensSeparadosDeQuantidade.includes('sanduiche')){
+        return true
+      }
+      
+    return false
+    }
+
+  verificarListaVazia(itens){
+      for(let prop in itens){
+          if(itens.hasOwnProperty(prop)){
+              return false
+          }
+      }
+      return true
+  }
+
+  verificarItemZero(itens){
+    for(let n of itens){
+      let teste = n[n.length - 1]
+      if(teste == 0){
+        return true
+      }
+    }
+    return false
+  }
+
+  calcularValorDaCompra(metodoDePagamento, itens) {
+      if(this.verificarAdicional(itens)){
+          return "Item extra não pode ser pedido sem o principal"
+      }
+
+      if(this.verificarListaVazia(itens)){
+          return "Não há itens no carrinho de compra!"
+      }
+
+      if(this.verificarItemZero(itens)){
+          return "Quantidade inválida!"
+      }
+      
+      return 'passou'
     }
 
 }
